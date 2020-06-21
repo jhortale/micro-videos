0e4de987952f39ada47c6120cd7c540f09f19f45
Joao Hortale <jhortale@gmail.com>
2020-06-21 04:47:36 -0300

    genres added

---
+++ app/Http/Controllers/Api/GenreController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\Genre;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class GenreController extends Controller
+{
+    private $rules = [
+        "name" => "required|max:255",
+        "is_active" => "boolean"
+    ];
+
+    public function index()
+    {
+        return Genre::all();
+    }
+
+    public function store(Request $request)
+    {
+        $this->validate($request, $this->rules);
+        return Genre::create($request->all());
+    }
+
+    public function show(Genre $genre)
+    {
+        return $genre;
+    }
+
+    public function update(Request $request, Genre $genre)
+    {
+        $this->validate($request, $this->rules);
+        $genre->update($request->all());
+        return $genre;
+    }
+
+    public function destroy(Genre $genre)
+    {
+        $genre->delete();
+        return response()->noContent(); //204 - No Content
+    }
+}
